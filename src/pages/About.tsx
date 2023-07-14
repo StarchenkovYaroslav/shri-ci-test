41aea66357196087167dd708596d22dcf7fb8e38
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2023-07-14 16:33:39 +0600

    update

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,7 +8,7 @@ export const About: FC = () => {
         <title>About page</title>
       </Helmet>
       <h1 data-testid="page-title">About</h1>
-      <div>NO HELLO NO NIGGA!</div>
+      <div>NO HELLO NO NIGGA!!</div>
       <p>This is an example application.</p>
     </>
   );
